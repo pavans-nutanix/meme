a2b25bae6c3b7d0a45ad5943eec098d1e420dc8d
pavans-nutanix <pavan.s@nutanix.com>
2020-10-10 17:42:05 +0530

    Updates

---
+++ precache-manifest.d1412e035e05f23b70b2ebf8204770f9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9569f68de6c0f0ce014",
+    "url": "/meme/static/css/main.6708d66c.chunk.css"
+  },
+  {
+    "revision": "d9569f68de6c0f0ce014",
+    "url": "/meme/static/js/main.49f310e5.chunk.js"
+  },
+  {
+    "revision": "41968b294926a2d87494",
+    "url": "/meme/static/js/runtime~main.1c7c63e9.js"
+  },
+  {
+    "revision": "1fcfbc93a677d15d42f5",
+    "url": "/meme/static/css/2.47de3b58.chunk.css"
+  },
+  {
+    "revision": "1fcfbc93a677d15d42f5",
+    "url": "/meme/static/js/2.ff3cbf98.chunk.js"
+  },
+  {
+    "revision": "f0abf41b597b09ba3c143649ecbe0e0c",
+    "url": "/meme/static/media/padubidri.f0abf41b.jpeg"
+  },
+  {
+    "revision": "1581f5fab64e4a1684f03ef3c969b134",
+    "url": "/meme/static/media/sad.1581f5fa.jpg"
+  },
+  {
+    "revision": "452b990b966094b7bbcf255c61cae604",
+    "url": "/meme/static/media/fry.452b990b.jpg"
+  },
+  {
+    "revision": "4e18f32875a58e497f10b353b13c8415",
+    "url": "/meme/static/media/jobs.4e18f328.jpg"
+  },
+  {
+    "revision": "11cac8cfa4f32943416da581cb09e0c3",
+    "url": "/meme/static/media/phone.11cac8cf.jpg"
+  },
+  {
+    "revision": "f242473074cfb69336b55fb1d5c3fc37",
+    "url": "/meme/static/media/wonka.f2424730.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/meme/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/meme/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/meme/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/meme/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/meme/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "330701e25ac819c9ff1d3bab009f9924",
+    "url": "/meme/index.html"
+  }
+];
